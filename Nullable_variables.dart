431e46fa45032082bf045cc951727cab63966547
DK <dongwook5478@gmail.com>
2023-03-30 18:00:22 +0900

    - Nullable Variables

--- Nullable_variables.dart
+++ Nullable_variables.dart
@@ -1,11 +1,17 @@
+/*
+      Nullable_variables.dart
+      To understand y null safety is necessary and how to use it
+      3-30-2023
+      Dounguk Kim
+ */
+
+// String을 보내야 되는데 0이라서 다음 진행 코드에 에러가 날 수 있다.
 void main(){
-  var dyna;
-  dyna=123;
-  print(dyna);
-  dyna="456";
-  print(dyna);
-  if (dyna is String){
-    dyna.length;
+  String? name="Dounguk Kim"; //null safety variable
+  name=null;
+  if (name!=null){    //way 1
+    name.isEmpty;
   }
-  print("asdf");
+  name?.isNotEmpty;   //way 2
+
 }
