37cb971ac60f1e6db6b1d85c23ff91e8d8ef1413
DK <dongwook5478@gmail.com>
2023-04-03 06:18:51 +0900

    add files on data types folder

---
+++ .idea/data types/Basic_Data_Type.dart
@@ -0,0 +1,19 @@
+/*
+      Basic_Data_Type.dart
+      basic data types i know and a new data type num
+      4-3-2023
+      Dounguk Kim
+ */
+void main(){
+  String name="Dounguk";
+  bool alive = ture;
+  int age= 29;
+  double number =1.1;
+
+
+  //  num can be int or double
+  num ether;
+  num=3;
+  num=1.3;
+
+}
---
+++ .idea/data types/Lists.dart
@@ -0,0 +1,21 @@
+/*
+      Basic_Data_Type.dart
+      study collection if, and how to creat list
+      4-3-2023
+      Dounguk Kim
+ */
+
+void main(){
+
+  var yes=true;
+  var test=[1,2,3,4,4, if (yes) 8]; //if문 해석: yes가 true면 8
+
+  if (yes==true){
+    test.add(88);
+  }
+
+
+
+  print(test);
+
+}
