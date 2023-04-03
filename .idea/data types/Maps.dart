43165bb3187cb4cd6f6b9a5abe82270625e5eb14
DK <dongwook5478@gmail.com>
2023-04-04 08:38:29 +0900

    -Maps.dart, Sets.dart

---
+++ .idea/data types/Maps.dart
@@ -0,0 +1,17 @@
+/*
+    Maps.dart
+    maps == dictionary: who to creat maps, and how to use it
+    4-4-2023
+    Dounguk Kim
+ */
+void main(){
+  var player={        //object == any kind of types
+    "name":"daniel",
+    "xp":19.99,
+    "superpower": false
+  };
+
+  Map<List<int>,bool> player2={
+    [1,2,3,4]:true
+  };
+}
---
+++ .idea/data types/Sets.dart
@@ -0,0 +1,11 @@
+/*
+    Set.dart
+    study sets
+    4-4-2023
+    Dounguk Kim
+ */
+void main(){
+  var num={1,2,3,4,5};
+  Set<int> num2={1,2,3,4,5};
+  print(num);
+}
