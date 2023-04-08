39085f7527629cb987c36d09897223f5c17d35fc
DK <dongwook5478@gmail.com>
2023-04-08 09:51:49 +0900

    add AbstractClasse : ; ; : ;

---
+++ .idea/classes/AbstractClasses.dart
@@ -0,0 +1,39 @@
+/*
+    ClassRecap.dart
+
+    4-8-2023
+    Dounguk Kim
+ */
+
+abstract class Human{
+  void walk();
+}
+
+class Player extends Human{
+  String name;
+  int xp;
+
+  Player({
+    required this.name,
+    required this.xp
+});
+
+  void walk(){
+    print("I am walking");
+  }
+}
+
+class Coach{
+
+  void walk()
+  {
+    print("Coach is walking");
+  }}
+
+void main(){
+  var me = Player(name: "Daniel", xp: 1230);
+  me.walk();
+
+  var coach=Coach();
+  coach.walk();
+}
